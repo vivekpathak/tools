6e7b62eb54a0d546824e22d5c6b1b6d8a39e9c1f
vivekpathak <vpathak+github@orgmeta.com>
2015-08-02 17:53:40 -0400

    compile into a single html file for loading speed test

--- divsync/htmlgen.py
+++ divsync/htmlgen.py
@@ -1,49 +1,93 @@
 #!/usr/bin/env python 
+# Compile an html page to inline all the scripts being referred by it.
+# Pass the non minified js code linked in the page via the closure compiler
+#
+# Notes:
+#
+# 1. Although there is a lot of innovative work done on asynchronous loading
+# of js includes at runtime, it seems that doing that work statically would result
+# in faster loading time because only one request would be sent.
+#
+# 2. The idea is to replace and inline the scripts and css into the page so
+# that the semantics of the html page would remain identical  
+#
+# Author : Vivek Pathak
+#          Sun Aug  2 16:48:38 EDT 2015
+#
+# Changes :
+#
+# 
 
 from bs4 import BeautifulSoup
-from jsmin import jsmin
 import tempfile
 import os
+import subprocess
 
 
-# create a subclass and override the handler methods
+
 class HTMLCompiler:
 
     def __init__(self,  filename) :
         self.basepath = os.path.dirname(os.path.realpath(filename))
         self.soup = BeautifulSoup( open(filename).read(), 'html.parser')
         self._replace_scripts()
+        self._replace_styles() 
         
     def output(self):
         return str(self.soup)
     
     def _replace_scripts(self) :
         for s in self.soup.find_all('script'):
-            if s.has_attr('src') and s['src'][0:4] != 'http' :
-                s.string = self._minify(self._inline_script_from_src(s['src']))
+
+            if s.has_attr('src') and s['src'][0:4] != 'http' :    # local script with src directive 
+                s.string = self._inline_minify_script_from_src(self.basepath + "/" + s['src'])
                 del s['src']
                 
+            elif not s.has_attr('src') and s.has_attr('type') and s['type'] == 'application/javascript' :  # inline script 
+                copyfd = tempfile.NamedTemporaryFile(delete=False)
+                copyfd.write(s.string)
+                copyfd.close()
+                s.string = self._inline_minify_script_from_src(copyfd.name)
+                del s['src']                
 
-    def _inline_script_from_src(self, filename):
-        fullfilename = self.basepath + "/" + filename 
-        return open(fullfilename).read()
-
-    def _minify(self, s):
-        return jsmin(s)
+    def _inline_minify_script_from_src(self, filename):
+        if filename[-6:] == 'min.js' :
+            return '\n' + open(filename).read() + '\n' 
+        else:
+            return '\n' + subprocess.check_output( [ "closure-compiler" , "--language_in=ECMASCRIPT5", "--compilation_level=WHITESPACE_ONLY", filename ] )  
     
-    def _minify2(self, s) :
-        src = tempfile.NamedTemporaryFile(delete = False)
-        src.write(s)
-        src.close()
-
-        tgt = tempfile.NamedTemporaryFile(delete = False)
-        os.system( "closure-compiler %s --js_output_file %s" % (src.name, tgt.name) )
-        smin = tgt.read()
-        os.system( "rm " + src.name + " " + tgt.name )  
-        return smin
 
 
+    def _replace_styles(self):
+        innerStyleTag = None 
+        styles = [] 
+        for s in self.soup.head.contents :
+            #print 'found ' , s.name
+            if not s.name:
+                pass
+            #if s.name == 'link' and s.has_attr('rel') and s['rel'] == 'stylesheet' and s.has_attr('href') and s['href'][0:4] != 'http' :
+            elif s.name == 'link' and s.has_attr('href') and s['href'][0:4] != 'http' :
+                styles.append(self._inline_minify_styles_from_src(self.basepath + "/" + s['href']))
+                del s
+            elif s.name == 'style' :
+                 innerStyleTag = s
+                 styles.append( s.string )
 
+        if innerStyleTag is None:
+            innerStyleTag = self.soup.new_tag("style", type="text/css")
+            self.soup.head.append( innerStyleTag )
+
+        innerStyleTag.string = '\n'.join( styles )
+        #print styles
+
+
+    def _inline_minify_styles_from_src(self, filename):
+        if filename[-7:] == 'min.css' :
+            return open(filename).read()
+        else:
+            return subprocess.check_output( [ "csstidy" , filename , "--silent=true" ] ) 
+        
+            
 
 if __name__ == "__main__":
     import sys
@@ -51,5 +95,8 @@ if __name__ == "__main__":
 
     parse = HTMLCompiler(src)
 
-    print( parse.output() )  
+    #print( parse.output() )  
 
+    target = sys.argv[2]
+    open( target , 'w' ).write( parse.output() )
+    
