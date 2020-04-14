3f076a68c7fad2d20a1be1b6515e1dad726a4ba8
vivekpathak <vpathak+github@orgmeta.com>
2020-04-13 21:58:59 -0400

    loop forever

---
+++ suspend.py
@@ -0,0 +1,28 @@
+#!/usr/in/env python
+"""
+run command once idleness exceeds that provided 
+"""
+import sys
+import time
+import subprocess
+
+SKIP_TRIES = 3
+MAX_SECONDS = 10*60 if len(sys.argv) == 1 else int(sys.argv[1])
+print("timeout %d seconds" % MAX_SECONDS)
+s = 0
+while True:
+    idlemillis = int(subprocess.check_output("xprintidle")) 
+    print(idlemillis)
+    time.sleep(15)
+    s += 1
+    if s > SKIP_TRIES and idlemillis > MAX_SECONDS*1000 :
+        print("suspend now")
+        try:
+            subprocess.check_call("systemctl suspend", shell=True)
+        except:
+            s = 0
+            thread.sleep(30)
+            pass
+
+
+
