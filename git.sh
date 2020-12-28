092fff0ba6efac717e1d2bd2ff40addecc2a3e64
vivekpathak <vpathak+github@orgmeta.com>
2020-12-28 03:27:01 -0500

    slightly improved structure simplified perhaps as a direction for decoupling

--- git.sh
+++ git.sh
@@ -1,29 +1,30 @@
 #!/bin/bash 
+
+
 pushd ~
 
-for dir in newlibertie/benchmarks newlibertie/docs newlibertie/web \
-	newlibertie/server newlibertie/voting-protocol \
-        clowdsource/cs-cui clowdsource/cs-db clowdsource/cs-rui clowdsource/cs-svc clowdsource/cs-admin \
-        pub/psychic-eureka \
-	pub/tools # TODO  newlibertie/db newlibertie/client pubcode/casters pubcode/authrep \
-do 
-	if ! pushd $dir ; then
-		echo exiting because of error at directory $dir
-		exit 
-	fi
+for dir in ~/pub/psychic-eureka/ ~/pub/tools ~/notes
+do
+    echo Working on $(pwd)
 
-	echo Working on $(pwd)
-	if ! git diff --quiet ; then 
-		echo Dirty : $dir .... skipping
-	else
-	    if ! git pull ; then
+    if pushd $dir ; then
+        echo OK                          # pushed $dir OK !
+    else
+        echo ERROR - failed to push $dir now at $(pwd)
+        exit 1
+    fi
+
+    if ! git diff --quiet ; then
+        echo Dirty : $dir .... skipping
+    else
+        if ! git pull ; then
 		    echo Conflict on pull for $dir .... reverting 
 		    git merge --abort
-	    else
+        else
 		    echo $dir is updated
         fi
-	fi 
-	popd 
+    fi
+    popd
 done
 
 popd
