0b8f9acc3c4bed7e0ac423c3cfacf763c0a9d2ea
vivekpathak <vpathak+github@orgmeta.com>
2021-06-13 12:49:52 -0400

    reduced noisiness and finished todo

--- git.sh
+++ git.sh
@@ -1,27 +1,29 @@
 #!/bin/bash 
 pushd ~
 
-for dir in newlibertie/benchmarks newlibertie/docs newlibertie/web \
+for dir in newlibertie/benchmarks newlibertie/db newlibertie/docs \
+				  newlibertie/web \
 	newlibertie/server newlibertie/voting-protocol \
-        clowdsource/cs-cui clowdsource/cs-db clowdsource/cs-rui clowdsource/cs-svc clowdsource/cs-admin \
+        clowdsource/cs-cui clowdsource/cs-db clowdsource/cs-rui \
+	clowdsource/cs-svc clowdsource/cs-admin \
         pub/psychic-eureka \
-	pub/tools # TODO  newlibertie/db newlibertie/client pubcode/casters pubcode/authrep \
+	pub/tools newlibertie/client pub/casters pub/authrep
 do 
 	if ! pushd $dir ; then
 		echo exiting because of error at directory $dir
 		exit 
 	fi
 
-	echo Working on $(pwd)
+	#echo Working on $(pwd)
 	if ! git diff --quiet ; then 
 		echo Dirty : $dir .... skipping
 	else
 	    if ! git pull ; then
 		    echo Conflict on pull for $dir .... reverting 
 		    git merge --abort
-	    else
-		    echo $dir is updated
-        fi
+	    #else
+		    #echo $dir is up to date
+            fi
 	fi 
 	popd 
 done
